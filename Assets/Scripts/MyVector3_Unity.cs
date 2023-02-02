67d9564d76c87f1820c2d7f6a3bf66782f25da4c
Kelvinfried <kelvinfriedchicken@gmail.com>
2023-02-02 07:28:16 +0000

    Added basic GameObject Functionality; One setup in the Unity Inspector, the other for if it is not setup as a backup.

--- Assets/Scripts/MyVector3_Unity.cs
+++ Assets/Scripts/MyVector3_Unity.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class MyVector3_Unity : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Making the target object public to be assigned inside of the editor
+    public GameObject target_GameObject;
+
+        // Start is called before the first frame update
     void Start()
     {
+
+
+            //If the target gameobject is not defined, use the slower GameObject.find to assign it.
+        if(target_GameObject== null )
+        {
+            Debug.Log("No game object, assigning manually." );
+
+                //Get player by their name and assign a referance to their GameObject.
+            string target_GameObject_name= "Player";
+                target_GameObject= GameObject.Find(target_GameObject_name );
+        }
+
+        else
+        {
+            Debug.Log("No need to assign.");
+        }
         
     }
 
-    // Update is called once per frame
+        // Update is called once per frame
     void Update()
     {
-        
+            //To ensure it works apply changes to it.
+        target_GameObject.transform.Rotate(100* Time.deltaTime, 0, 0 );
     }
 }
